7bd7acc9385a729c8f02e9f79f8d0d19ba58b851
1N0T <toni.solis.naranjo@gmail.com>
2019-01-15 20:15:15 +0100

    travis pytest

--- HelloWorldTest.py
+++ HelloWorldTest.py
@@ -9,8 +9,9 @@ class HelloWorldTest(unittest.TestCase):
 	def test_greeting(self):
 		saludo = HelloWorld()
 		self.assertEqual(saludo.message, "Hello World !!!")
-		
-		
+	def test_greeting_2(self):
+		saludo = HelloWorld()
+		self.assertNotEqual(saludo.message, "Hola Mundo !!!")
+
 if __name__ == "__main__":
 	unittest.main()
-	
